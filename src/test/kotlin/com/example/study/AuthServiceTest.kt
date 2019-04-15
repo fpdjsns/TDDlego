9fb205c4ce0897d15eb5a0c7644a3e0b0aa0aa69
배현수 <withham1@naver.com>
2019-04-15 21:39:41 +0900

    ~ 25:30

--- src/test/kotlin/com/example/study/AuthServiceTest.kt
+++ src/test/kotlin/com/example/study/AuthServiceTest.kt
@@ -4,14 +4,15 @@ import org.hamcrest.Matchers.instanceOf
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.*
 
 class AuthServiceTest {
-    var authService: AuthService? = null
     val USER_PASSWORD = "userPassword"
+    val mockUserRepository: UserRepository = mock(UserRepository::class.java)
+    val authService: AuthService = AuthService(mockUserRepository)
 
     @Before
-    fun canCreate() {
-        authService = AuthService()
+    fun setUp() {
     }
 
     @Test
@@ -22,18 +23,59 @@ class AuthServiceTest {
         assertIllegalArgExThrown("userId", "")
     }
 
+    @Test
+    fun whenUserNotFound_throwNonExistingUserEx(){
+        assertExceptionThrown("noUserId", USER_PASSWORD, NonExistingUserException::class.java)
+        assertExceptionThrown("noUserId2", USER_PASSWORD, NonExistingUserException::class.java)
+    }
+
+    @Test
+    fun whenUserFoundButWrongPw_throwWrongPasswordEx() {
+        givenUserExists("userId", USER_PASSWORD)
+        assertExceptionThrown("userId", "wrongPassword", WrongPasswordException::class.java)
+        verifyUserFound("userId")
+    }
+
+    private fun givenUserExists(id: String, password: String) {
+        `when`(mockUserRepository.findById(id)).thenReturn(User(id, password))
+    }
+
+    private fun verifyUserFound(id: String) {
+        verify(mockUserRepository).findById(id)
+    }
+
+    class UserRepository {
+        fun findById(id: String): User {
+            return User(id)
+        }
+
+    }
+
+    class WrongPasswordException : Exception(){
+
+    }
+
+    class NonExistingUserException : Exception(){
+
+    }
+
     private fun assertIllegalArgExThrown(id: String?, password: String?) {
+        assertExceptionThrown(id, password, IllegalArgumentException::class.java)
+    }
+
+    private fun assertExceptionThrown(id: String?, password: String?, type: Class<out Exception>) {
         var thrownEx: Exception? = null
         try {
             authService!!.authenticate(id, password)
         } catch (e: Exception) {
             thrownEx = e
         }
-        assertThat(thrownEx, instanceOf(IllegalArgumentException::class.java))
+        assertThat(thrownEx, instanceOf(type))
     }
 
 
-    class AuthService {
+    class AuthService(var userRepository: UserRepository) {
+
         fun authenticate(id: String?, password: String?) {
             if(id.isNullOrEmpty()){
                 throw IllegalArgumentException()
@@ -41,6 +83,19 @@ class AuthServiceTest {
             if(password.isNullOrEmpty()){
                 throw IllegalArgumentException()
             }
+
+            val user: User = findUserById(id)
+                    ?: throw NonExistingUserException()
+            throw WrongPasswordException()
+        }
+
+        private fun findUserById(id: String): User? {
+            return userRepository.findById(id)
+//            if(id.equals("userId"))
+//                return User(id, "1234")
+//            return null
         }
     }
+
+    data class User(val id: String, val password: String? = null)
 }
